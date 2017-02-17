b62a91a5f6d890acb879da2c192b754e7a9924a4
pszklanko <pszklanko@gmail.com>
2017-02-17 15:46:43 +0100

    first steps

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@
  * Here initialization of scripts
  * Library please put in separate file.
  */
+ $('document').ready(function () {
+   var picturesArray = $("img").map(function () {
+        return $(this).attr("src");
+    }).get();
+    var itemNum = 1;
+    $('li').first().addClass('mainImg');
+    $('.mainImg').on('click', function () {
+      $('.mainImg').fadeOut('slow', function () {
+
+        $('.mainImg img').attr('src', picturesArray[itemNum++%picturesArray.length]);
+      }).fadeIn(1000);
+    })
+});

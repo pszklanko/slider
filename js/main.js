142e917e278413e5bce45b9150500bdc08063fbe
pszklanko <pszklanko@gmail.com>
2017-02-21 16:19:52 +0100

    separated slider source

--- js/main.js
+++ js/main.js
@@ -2,53 +2,6 @@
  * Here initialization of scripts
  * Library please put in separate file.
  */
- 'use strict';
  $('document').ready(function () {
-  $('li:gt(0)').hide();
-  //$('li:first').addClass('mainImg');
-  var width = $('img').width();
-  var stripes = 8;
-  var stripeWidth = width/stripes;
-  var totalWidth = 0;
-
-  for (var i = 0; i < stripes; i++) {
-     $('ul').append('<span style="left:'+ i * stripeWidth +'px"></span>');
-   }
-
-   setInterval(function() {
-     sliceImg();
-     $('span').each(function() {
-       totalWidth += $(this).width();
-     })
-     if(totalWidth===800) {
-       totalWidth = 0;
-       if($('span:visible').length === 8){
-         toggleImgs();
-       }
-     }
-   }, 3000);
+   $().slider(3000);
 });
-
-function sliceImg() {
-  var collection = $('ul span');
-  if( collection.length > 0 ){
-    var i = 0;
-    var fn = function(){
-        var element = $(collection[i]);
-        element.fadeToggle(1000);
-        if( ++i < collection.length ){
-          setTimeout(fn, 200);
-        }
-    };
-    fn();
-}
-}
-
-function toggleImgs() {
-    $('li:first')
-       .toggle()
-       .next()
-       .toggle()
-       .end()
-       .insertBefore('span:first');
-}
